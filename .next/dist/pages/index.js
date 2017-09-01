be904d2bcef9dd1e7aa22d7617fc4e257e20e8d2
Jane <zjgoing@gmail.com>
2017-09-01 19:08:29 +0800

    issue: add route

--- .next/dist/pages/index.js
+++ .next/dist/pages/index.js
@@ -8,16 +8,41 @@ var _react = require("react");
 
 var _react2 = _interopRequireDefault(_react);
 
+var _link = require("next/dist/lib/link.js");
+
+var _link2 = _interopRequireDefault(_link);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _jsxFileName = "/Users/feizheng/github/nextjs-ssr/pages/index.js?entry";
+
+
+var linkStyle = {
+  marginRight: 15
+};
 
 exports.default = function () {
   return _react2.default.createElement("div", {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 2
+      lineNumber: 8
     }
-  }, "Welcome to next.js!");
+  }, _react2.default.createElement(_link2.default, { href: "/", __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    }
+  }, _react2.default.createElement("a", { style: linkStyle, __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    }
+  }, "Home")), _react2.default.createElement(_link2.default, { href: "/about", __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    }
+  }, _react2.default.createElement("a", { style: linkStyle, __source: {
+      fileName: _jsxFileName,
+      lineNumber: 13
+    }
+  }, "About")), "Welcome to next.js!");
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBOzs7Ozs7OztrQkFBZSxZQUFBO3lCQUNiLGNBQUE7O2dCQUFBO2tCQUFBO0FBQUE7QUFBQSxHQUFBLEVBRGEsQUFDYjtBQURGIiwiZmlsZSI6ImluZGV4LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6Ii9Vc2Vycy9mZWl6aGVuZy9naXRodWIvbmV4dGpzLXNzciJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIkxpbmsiLCJsaW5rU3R5bGUiLCJtYXJnaW5SaWdodCJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7OztBQUFBLEFBQU87Ozs7Ozs7OztBQUVQLElBQU07ZUFBTixBQUFrQixBQUNILEFBR2Y7QUFKa0IsQUFDaEI7O2tCQUdhLFlBQUE7eUJBQ2IsY0FBQTs7Z0JBQUE7a0JBQUEsQUFDRTtBQURGO0FBQUEsR0FBQSxrQkFDRSxBQUFDLGdDQUFLLE1BQU4sQUFBVztnQkFBWDtrQkFBQSxBQUNFO0FBREY7cUJBQ0UsY0FBQSxPQUFHLE9BQUgsQUFBVTtnQkFBVjtrQkFBQTtBQUFBO0tBRkosQUFDRSxBQUNFLEFBRUYsMEJBQUEsQUFBQyxnQ0FBSyxNQUFOLEFBQVc7Z0JBQVg7a0JBQUEsQUFDRTtBQURGO3FCQUNFLGNBQUEsT0FBRyxPQUFILEFBQVU7Z0JBQVY7a0JBQUE7QUFBQTtLQUxKLEFBSUUsQUFDRSxXQU5TLEFBQ2I7QUFERiIsImZpbGUiOiJpbmRleC5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiIvVXNlcnMvZmVpemhlbmcvZ2l0aHViL25leHRqcy1zc3IifQ==
